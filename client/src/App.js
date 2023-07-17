745584904838747bea4f67b1a99078524a3f57a5
DDing <jangmee9918@gmail.com>
2023-07-17 23:07:41 +0900

    WebRTC틀 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-function ListClicked(){
-  console.log("방제클릭됌");
-}
+let cnt=0;
 function List(props){
-  return(<li onClick={ListClicked}>{props.title}
+  return(<li onClick={ async ()=>{
+    props.onClick("유저"+cnt,props.index);
+    const video=document.getElementById("videoElement");
+    const constraints = window.constraints = {
+      audio: true,
+      video: true
+    };
+    try{
+      const stream=await navigator.mediaDevices.getUserMedia(constraints);
+      console.log("받아옴");
+      video.srcObject=stream;
+    }catch(e){
+      console.error(e);
+    }
+  }
+  }>{props.title}
   <div>
-        <div display="inline-block">
-      {props.Users.map((User)=>(
-        <img src={logo} className="App-logo" alt="logo"/>
-          
-      ))}</div>
+    {console.log(props.index,"번방",props.RoomData.length)}
+          {props.RoomData.map(value=>{
+            //console.log(props.index,"번방",value);
+            //console.log("확인용",value[1],props.index,value[1]==props.index,value[1]===props.index);
+            if(value[1]==props.index){
+              return (<div style={{ display: "flex", alignItems: "center" }} display="inline-block" key={value[0]}>
+              <img src={logo} className="App-logo" alt="logo"/>
+              <div>{value[0]}</div>
+              </div>)
+            }
+          })}
     </div>
     </li>);
 }
@@ -20,25 +39,25 @@ function Btn(props){
 }
 function App() {
   const [Room,modifiRoom]=React.useState([]);
-  const [User,modifiUser]=React.useState([]);
 
-  const handleRoom=() =>{
-    const newRoom=[...Room,`${Room.length+1}`];
+  const handleRoom=(name,RoomNum) =>{
+    const newRoom=[...Room,[name,RoomNum]];
+    //console.log(newRoom);
+    cnt++;
     modifiRoom(newRoom);
-  }
-  const handleUser =()=>{
-    const newUser=[...User,`${User.length+1}`];
-    modifiUser(newUser);
   }
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          {Room.map((title,index)=>(
-            <List title={title} key={index} Users={User}/>
-          ))}
-          <Btn title="임시 사용자추가" onClick={handleUser} />
-          <Btn title="임시 방추가" onClick={handleRoom} />
+        <ul className="chatroom">
+            <List onClick={handleRoom} title={"1번 방"} key={1} index={1} RoomData={Room}/>
+            <List onClick={handleRoom}title={"2번 방"} key={2} index={2} RoomData={Room}/>
+            <List onClick={handleRoom}title={"3번 방"} key={3} index={3} RoomData={Room}/>
+            <List onClick={handleRoom}title={"4번 방"} key={4} index={4} RoomData={Room}/>
+            <List onClick={handleRoom}title={"5번 방"} key={5} index={5} RoomData={Room}/>
+        </ul>
+        <ul className="viewroom">
+          <video id="videoElement" autoPlay></video>
         </ul>
       </header>
     </div>
